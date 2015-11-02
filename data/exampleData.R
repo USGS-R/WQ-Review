9bcf7b41408164872c7f65593682df9859e29503
T. Joe Mills <tmills@usgs.gov>
2015-11-02 10:13:50 -0700

    doc update

--- data/exampleData.R
+++
@@ -1,8 +0,0 @@
-#' @name qw.data
-#' @title Output qw.data object for the Big Thompson River generated using readNWISodbc.
-#' @description This contains all data for the Big Thompson River site number 06733000 pulled from NWIS using the readNWISodbc function. 
-#' This dataset is used for all run examples.
-#' @docType data
-#' @usage qw.data
-#' @source Colorado NWIS server
-#' @author Joe Mills, 2015-11-02
