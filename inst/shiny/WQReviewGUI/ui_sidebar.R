199a5d0c70775fe41945c06f6cfba17411c49ce7
T. Joe Mills <tmills@usgs.gov>
2016-05-24 16:37:36 -0600

    sidebar reorg

--- inst/shiny/WQReviewGUI/ui_sidebar.R
+++ inst/shiny/WQReviewGUI/ui_sidebar.R
@@ -3,16 +3,16 @@
 
 dashboardSidebar(sidebarMenu(
         
-                
-
-                        
-                       
-                
-                       ###Load the controls
+        
+        
+        
+        
+        
+        ###Load the controls
         menuItem("Data import",
                  menuSubItem(tabName="siteSelection", text = "Import sites and samples"),
                  menuSubItem(tabName="nwisDCImport", text = "NWIS Datachecks import")
-                 ),
+        ),
         
         menuItem("Chemical sense checks",icon = icon("table"),
                  menuSubItem(tabName="chemFlagTable","Chemical sense flags",icon = icon("table")),
@@ -20,91 +20,91 @@ dashboardSidebar(sidebarMenu(
                  menuSubItem(tabName="cbPlot",text="Chargebalance plot",icon = icon("bar-chart")),
                  menuSubItem(tabName="scSumPlot",text="Ions vs conductance plot",icon = icon("bar-chart"))
                  
-                 ),
+        ),
         
         menuItem("Pesticide checks",icon = icon("table"),
                  menuSubItem(tabName="pestFlagTable","Pesticide flags",icon = icon("table"))
         ),
         
         menuItem("Bias analysis",icon = icon("table"),
-        menuItem("Blank analysis",
-                 menuSubItem(tabName="blankTable","Blank summary",icon = icon("table")),
-                 menuSubItem(tabName="parmBoxPlot",text="Parameter boxplot",icon = icon("bar-chart")),
-                 menuSubItem(tabName="blankPlot",text="Blank timeseries",icon = icon("bar-chart"))
-        ),
-        
-        menuItem("Replicate analysis",
-                 menuSubItem(tabName="repTable","Replicate table",icon = icon("table")),
-                 menuSubItem(tabName="repBoxPlot",text="Replicate boxplot",icon = icon("bar-chart"))
-        )
-        ),
-        
-        
-                menuItem("Plots",
-                         
-                         dateInput("newThreshold", "New samples threshold",max=Sys.Date(),value=Sys.Date()-30),
-                         #textInput("recordSelect", "Highlight record #"),
-                         
-                           
-                         menuSubItem(tabName="timeSeries",text="Timeseries"),
-                         
-                         menuSubItem(tabName="seasonalPlot",text="Seasonal"),
-                         
-                         menuSubItem(tabName="parmBoxPlot",text="Parameter boxplot"),
-                         
-                         menuSubItem(tabName="parmParmPlot",text="Parameter vs. parameter plot"),
-                         
-                         menuSubItem(tabName="matrixPlot",text="Matrix plot"),
-                         
-                         
-                         
-                         
-                         
-                         
-                         
-                         #menuSubItem(tabName="mapPlot",text="Spatial plot"),
-                         
-                                                   
-                         ###Other arguments to top menu item
-                          tabName = "Plots", 
-                          icon = icon("bar-chart"),selected=FALSE
-                         ),
-                 ###Next sidebar entry
-                 menuItem("QAQC Tables", 
-                          ###Table types, each one will appear in dashboard
-                          
-                          menuSubItem(tabName="balanceTable","Charge balance table"),
-                          
-                          
-                          menuSubItem(tabName="wholvevpartTable","Fil vs. Unf table"),
-                          ###Other arguments ot top menu item                          
-                          tabName = "QAQCTables", icon = icon("table")
-                          ),
-                menuItem("Data Tables",
-                         menuSubItem(tabName="wideDataTable","Wide data table"),
-                         menuSubItem(tabName="longDataTable","Long data table"),
-                         tabName = "dataTables", icon = icon("table")
-                ),
-        menuItem("Reviewer notes",
-                 h3("Review comments"),
-                 textInput("sidebar_flaggedRecord",label="Record #"),
-                 textInput("sidebar_flaggedComment",label = "Comment"),
-                 actionButton(inputId = "sidebar_addRecord",label="Add record"),
-                 
-                 menuSubItem(tabName="markedRecordsTab","View table")
-                 
+                 menuItem("Blanks",
+                          menuSubItem(tabName="blankTable","Blank summary",icon = icon("table")),
+                          menuSubItem(tabName="parmBoxPlot",text="Parameter boxplot",icon = icon("bar-chart")),
+                          menuSubItem(tabName="blankPlot",text="Blank timeseries",icon = icon("bar-chart"))
                  ),
+                 
+                 menuItem("Replicates",
+                          menuSubItem(tabName="repTable","Replicate table",icon = icon("table")),
+                          menuSubItem(tabName="repBoxPlot",text="Replicate boxplot",icon = icon("bar-chart"))
+                 ),
+                 
+                 menuItem("Fil. vs Unf.",
+                          menuSubItem(tabName="wholvevpartTable","Fil vs. Unf table",icon = icon("table"))
+                 )
+        ),
         
-                
-        #menuSubItem(tabName="dataUpload","Data upload tools"),
-        #menuSubItem(tabName="srsSummary","SRS Summary tool"),
-        menuItem(tabName="saveLoadTab","Save/load data"),
-        menuItem(tabName="helpTab","Help",
-                menuSubItem(href="FAQ.htm",text="FAQ"),
-                menuSubItem(href="WQReviewGUI.html",text="User Guide")
-        )
+        menuItem("Result-level checks",icon = icon("table"),
+                 menuSubItem(tabName="resultFlagTable","Result-level flags",icon = icon("table")),
+                 menuItem("Plots",
+                             
+                             dateInput("newThreshold", "New samples threshold",max=Sys.Date(),value=Sys.Date()-30),
+                             #textInput("recordSelect", "Highlight record #"),
+                             
+                             
+                             menuSubItem(tabName="timeSeries",text="Timeseries"),
+                             
+                             menuSubItem(tabName="seasonalPlot",text="Seasonal"),
+                             
+                             menuSubItem(tabName="parmBoxPlot",text="Parameter boxplot"),
+                             
+                             menuSubItem(tabName="parmParmPlot",text="Param vs. Param plot"),
+                             
+                             menuSubItem(tabName="matrixPlot",text="Matrix plot"),
+                             
+                             
+                             
+                             
+                             
+                             
+                             
+                             #menuSubItem(tabName="mapPlot",text="Spatial plot"),
+                             
+                             
+                             ###Other arguments to top menu item
+                             tabName = "Plots", 
+                             icon = icon("bar-chart"),selected=FALSE
+                 )
+                 
+        ),
         
-       
-        
-        )
+
+
+
+menuItem("Data Tables",
+         menuSubItem(tabName="wideDataTable","Wide data table"),
+         menuSubItem(tabName="longDataTable","Long data table"),
+         tabName = "dataTables", icon = icon("table")
+),
+menuItem("Reviewer notes",
+         h3("Review comments"),
+         textInput("sidebar_flaggedRecord",label="Record #"),
+         textInput("sidebar_flaggedComment",label = "Comment"),
+         actionButton(inputId = "sidebar_addRecord",label="Add record"),
+         
+         menuSubItem(tabName="markedRecordsTab","View table")
+         
+),
+
+
+#menuSubItem(tabName="dataUpload","Data upload tools"),
+#menuSubItem(tabName="srsSummary","SRS Summary tool"),
+menuItem(tabName="saveLoadTab","Save/load data"),
+menuItem(tabName="helpTab","Help",
+         menuSubItem(href="FAQ.htm",text="FAQ"),
+         menuSubItem(href="WQReviewGUI.html",text="User Guide")
+)
+
+
+
+)
 )
