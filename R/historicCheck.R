a3f33b274d2f01a29d2086ba72840994eaca6bcf
T. Joe Mills <tmills@usgs.gov>
2016-10-12 13:35:25 -0600

    Fixes #110

--- R/historicCheck.R
+++ R/historicCheck.R
@@ -27,84 +27,110 @@ historicCheck <- function(qw.data, returnAll = FALSE)
         
         if(nrow(inReviewData) > 0)
         {
-        #Get stats by parm for each site
-        siteStats <- dplyr::summarize(group_by(approvedData,SITE_NO,PARM_CD),
-                                              min = min(RESULT_VA,na.rm=TRUE),
-                                              max = max(RESULT_VA,na.rm=TRUE),
-                                              quant99 = quantile(RESULT_VA,probs=(0.99),na.rm=TRUE),
-                                              quant01 = quantile(RESULT_VA,probs=(0.01),na.rm=TRUE),
+                #Get stats by parm for each site
+                siteStats <- dplyr::summarize(group_by(approvedData,SITE_NO,PARM_CD),
+                                              min = min(RESULT_VA[REMARK_CD != "<"],na.rm=TRUE),
+                                              max = max(RESULT_VA[REMARK_CD != ">"],na.rm=TRUE),
+                                              percNonDetects = length(na.omit(RESULT_VA[REMARK_CD == "<"]))/length(na.omit(RESULT_VA))*100,
+                                              quant99 = quantile(RESULT_VA[REMARK_CD != ">"],probs=(0.99),na.rm=TRUE),
+                                              quant01 = quantile(RESULT_VA[REMARK_CD != "<"],probs=(0.01),na.rm=TRUE),
                                               N = length(RESULT_VA)
                 )
-        
-        inReviewData <- dplyr::left_join(inReviewData,siteStats, by = c("SITE_NO","PARM_CD"))
-        
-        ##Check if new max
-        inReviewData$newMax_30.11 <- NA
-        inReviewData$newMax_30.11[which(inReviewData$RESULT_VA > inReviewData$max &
-                                          inReviewData$N > 4)] <-
-                paste("flag",
-                      inReviewData$RESULT_VA[which(inReviewData$RESULT_VA > inReviewData$max &
-                                                           inReviewData$N > 4)]
-                )
-                   
-        ##Check if new min
-        inReviewData$newMin_30.12 <- NA
-        inReviewData$newMin_30.12[which(inReviewData$RESULT_VA < inReviewData$min &
-                                          inReviewData$N > 4)] <-
-                paste("flag",
-                      inReviewData$RESULT_VA[which(inReviewData$RESULT_VA < inReviewData$min &
-                                                           inReviewData$N > 4)]
-                )  
-        
-        ##Check if greater than 99th percentile
-        inReviewData$greaterQuant99_30.15 <- NA
-        inReviewData$greaterQuant99_30.15[which(inReviewData$RESULT_VA > inReviewData$quant99 &
-                                          inReviewData$N > 4)] <-
-                paste("flag",
-                      inReviewData$RESULT_VA[which(inReviewData$RESULT_VA > inReviewData$quant99 &
-                                                           inReviewData$N > 4)]
-                )
-        
-        ##Check if less than 1th percentile
-        inReviewData$lessQuant01_30.16 <- NA
-        inReviewData$lessQuant01_30.16[which(inReviewData$RESULT_VA < inReviewData$quant01 &
-                                          inReviewData$N > 4)] <-
-                paste("flag",
-                      inReviewData$RESULT_VA[which(inReviewData$RESULT_VA < inReviewData$quant01 &
-                                                           inReviewData$N > 4)]
-                )
-        
-        #Extract site info and flags
-        flaggedSamples <- inReviewData[c("RECORD_NO",
-                                         "SITE_NO",
-                                         "STATION_NM",
-                                         "SAMPLE_START_DT",
-                                         "SAMPLE_END_DT",
-                                         "MEDIUM_CD",
-                                         "PARM_CD",
-                                         "PARM_NM",
-                                         "PARM_SEQ_GRP_CD",
-                                         "min",
-                                         "max",
-                                         "quant99",
-                                         "quant01",
-                                         "newMax_30.11",
-                                         "newMin_30.12",
-                                         "greaterQuant99_30.15",
-                                         "lessQuant01_30.16"
-                                         )]
-        if(returnAll == FALSE)
-        {
-        #remove NAs from result flags
-        flaggedSamples <- unique(flaggedSamples[which(!is.na(flaggedSamples[14]) |
-                                                            !is.na(flaggedSamples[15]) |
-                                                            !is.na(flaggedSamples[16]) |
-                                                            !is.na(flaggedSamples[17])
-        ),]) 
-        } else {}
-        
-        return(unique(flaggedSamples))
-        
+                inReviewData <- dplyr::left_join(inReviewData,siteStats, by = c("SITE_NO","PARM_CD"))
+                
+                ##Check if new max
+                inReviewData$newMax_30.11 <- NA
+                inReviewData$newMax_30.11[is.finite(inReviewData$max) &
+                                                  inReviewData$REMARK_CD != ">" & 
+                                                  inReviewData$RESULT_VA > inReviewData$max &
+                                                  inReviewData$N > 4] <-
+                        paste0("flag_",
+                               inReviewData$RESULT_VA[is.finite(inReviewData$max) &
+                                                              inReviewData$REMARK_CD != ">" &
+                                                              inReviewData$RESULT_VA > inReviewData$max &
+                                                              inReviewData$N > 4]
+                        )
+
+                ##Check if new min
+                inReviewData$newMin_30.12 <- NA
+                inReviewData$newMin_30.12[is.finite(inReviewData$min) &
+                                                  inReviewData$REMARK_CD != "<" & 
+                                                  inReviewData$RESULT_VA < inReviewData$min &
+                                                  inReviewData$N > 4] <-
+                        paste("flag",
+                              inReviewData$RESULT_VA[is.finite(inReviewData$min) &
+                                                             inReviewData$REMARK_CD != "<" & 
+                                                             inReviewData$RESULT_VA < inReviewData$min &
+                                                             inReviewData$N > 4]
+                        )  
+                
+                ##Check if greater than 99th percentile
+                inReviewData$greaterQuant99_30.15 <- NA
+                inReviewData$greaterQuant99_30.15[is.finite(inReviewData$quant99) &
+                                                          inReviewData$REMARK_CD != ">" & 
+                                                          inReviewData$RESULT_VA > inReviewData$quant99 &
+                                                          inReviewData$N > 4] <-
+                        paste("flag",
+                              inReviewData$RESULT_VA[is.finite(inReviewData$quant99) &
+                                                             inReviewData$REMARK_CD != ">" &
+                                                             inReviewData$RESULT_VA > inReviewData$quant99 &
+                                                             inReviewData$N > 4]
+                        )
+                
+                ##Check if less than 1th percentile
+                inReviewData$lessQuant01_30.16 <- NA
+                inReviewData$lessQuant01_30.16[is.finite(inReviewData$quant01) &
+                                                       inReviewData$REMARK_CD != "<" & 
+                                                       inReviewData$RESULT_VA < inReviewData$quant01 &
+                                                       inReviewData$N > 4] <-
+                        paste("flag",
+                              inReviewData$RESULT_VA[is.finite(inReviewData$quant01) &
+                                                             inReviewData$REMARK_CD != "<" &
+                                                             inReviewData$RESULT_VA < inReviewData$quant01 &
+                                                             inReviewData$N > 4]
+                        )
+                
+                ##Check if unusual non detect
+                inReviewData$unusualNonDetect <- NA
+                inReviewData$unusualNonDetect[is.finite(inReviewData$percNonDetects) &
+                                                      inReviewData$REMARK_CD == "<" &
+                                                      inReviewData$percNonDetects < 5 &
+                                                      inReviewData$N > 4] <- "flag"
+                
+                #Extract site info and flags
+                flaggedSamples <- inReviewData[c("RECORD_NO",
+                                                 "SITE_NO",
+                                                 "STATION_NM",
+                                                 "SAMPLE_START_DT",
+                                                 "SAMPLE_END_DT",
+                                                 "MEDIUM_CD",
+                                                 "PARM_CD",
+                                                 "PARM_NM",
+                                                 "RESULT_VA",
+                                                 "REMARK_CD",
+                                                 "PARM_SEQ_GRP_CD",
+                                                 "min",
+                                                 "max",
+                                                 "quant99",
+                                                 "quant01",
+                                                 "newMax_30.11",
+                                                 "newMin_30.12",
+                                                 "greaterQuant99_30.15",
+                                                 "lessQuant01_30.16",
+                                                 "unusualNonDetect"
+                )]
+                if(returnAll == FALSE)
+                {
+                        #remove NAs from result flags
+                        flaggedSamples <- unique(flaggedSamples[which(!is.na(flaggedSamples[14]) |
+                                                                              !is.na(flaggedSamples[15]) |
+                                                                              !is.na(flaggedSamples[16]) |
+                                                                              !is.na(flaggedSamples[17])
+                        ),]) 
+                } else {}
+                
+                return(unique(flaggedSamples))
+                
         } else{ print("No in-review data for comparison to approved data")
                 return(data.frame(RECORD_NO = NA,
                                   SITE_NO = NA,
