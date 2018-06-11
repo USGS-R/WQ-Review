5dbaca9ebd25d6a724b442f2ef9599164a51b972
Colin Penn <cpenn@usgs.gov>
2018-06-11 13:15:56 -0600

    remove str_wrap on title/axis labels causing visual issues in renderPlotly/shinyapp

--- R/qwparmparmPlot.R
+++ R/qwparmparmPlot.R
@@ -38,116 +38,120 @@
 
 
 qwparmParmPlot <- function(qw.data,
-                      site.selection,
-                      xparm,
-                      yparm,
-                      facet = "multisite",
-                      scales="fixed",
-                      new.threshold = 60*60*24*30,
-                      show.lm = FALSE,
-                      log.scaleY = FALSE,
-                      log.scaleX = FALSE,
-                      highlightrecords = " ",
-                      wySymbol = FALSE,
-                      labelDQI=FALSE,
-                      printPlot=TRUE){
-  
-  lm_eqn = function(df){
-    m = lm(RESULT_VA_Y ~ RESULT_VA_X, df);
-    eq <- paste("y =",format(coef(m)[2], digits = 2),"x +",format(coef(m)[1], digits = 2),"\nr-squared =",format(summary(m)$r.squared, digits = 3))
-    
-  }
-  
-  ## Sets color to medium code name, not factor level, so its consistant between all plots regardles of number of medium codes in data
-  medium.colors <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#D55E00")
-  names(medium.colors) <- c("WS ","WG ","WSQ","WGQ","OAQ")
-  
-  if(length(site.selection) == 1)
-  {
-    maintitle <- str_wrap(unique(qw.data$PlotTable$STATION_NM[which(qw.data$PlotTable$SITE_NO == (site.selection))]), width = 25)
-  } else if (length(site.selection) > 1)
-  {
-    maintitle <- "Multisite plot"
-  } else (maintitle <- "No site selected")
-  
-  ###Get X and Y labels from NWIS parm name
-  xlabel <- str_wrap(unique(qw.data$PlotTable$PARM_DS[which(qw.data$PlotTable$PARM_CD==(xparm))]), width = 25)
-  ylabel <- str_wrap(unique(qw.data$PlotTable$PARM_DS[which(qw.data$PlotTable$PARM_CD==(yparm))]), width = 25)
-  ###Subset data to parms and join by record number
-  ###This is very ugly but I don't know a way to pair up the x-y data in a melted dataframe
-  ###Subsetting by parm code does not work because you need the parmcodes matched up for the same record
-  
-  xpp.plot.data <- subset(qw.data$PlotTable,SITE_NO %in% (site.selection) & PARM_CD==(xparm))
-  ypp.plot.data <- subset(qw.data$PlotTable,SITE_NO %in% (site.selection) & PARM_CD==(yparm))
-  
-  ###Assigned to global environment to make it work with ggplot2, I don't like doing this since it is not a persistant variable
-  ###but this is hte fastest fix for now
-  pp.plot.data <- dplyr::left_join(xpp.plot.data[,c("RECORD_NO","SITE_NO","STATION_NM","MEDIUM_CD","SAMPLE_START_DT","RESULT_VA","RESULT_MD","DQI_CD")], 
-                        ypp.plot.data[,c("RECORD_NO","RESULT_VA","RESULT_MD","DQI_CD")],by="RECORD_NO")
-  names(pp.plot.data) <- c("RECORD_NO","SITE_NO","STATION_NM","MEDIUM_CD","SAMPLE_START_DT","RESULT_VA_X","RESULT_MD_X","DQI_CD_X","RESULT_VA_Y","RESULT_MD_Y","DQI_CD_Y")
-  remove(xpp.plot.data)
-  remove(ypp.plot.data)
-  
-  pp.plot.data <- na.omit(pp.plot.data)
-  ###Make the plot
-  p1 <- ggplot(data=pp.plot.data)
-  p1 <- p1 + geom_point(aes(x=RESULT_VA_X,y=RESULT_VA_Y,color = MEDIUM_CD),size=3)
-  p1 <- p1 + ylab(paste(ylabel,"\n")) + xlab(paste("\n",xlabel))
-  #Highlight records
-  if(nrow(subset(pp.plot.data, RECORD_NO %in% highlightrecords)) > 0)
-  {
-          p1 <- p1 + geom_point(data=subset(pp.plot.data,RECORD_NO %in% highlightrecords),aes(x=RESULT_VA_X,y=RESULT_VA_Y),size=7,alpha=0.5, color = "#D55E00",shape=19)
-  } else{}
-  
-  p1 <- p1 + scale_colour_manual("Medium code",values = medium.colors)
-  if ( facet == "Facet")
-  {
-  p1 <- p1 + facet_wrap(~ STATION_NM, nrow = 1, scales=scales) 
-  }else{}
-  #p1 <- p1 + stat_ellipse(aes(x=RESULT_VA_X,y=RESULT_VA_Y),level=0.999,type="t")
-  if(log.scaleY == TRUE)
-  {
-    p1 <- p1 + scale_y_log10()
-  }
-  if(log.scaleX == TRUE)
-  {
-    p1 <- p1 + scale_x_log10()
-  }
-
-  ##Check for new samples and label them. Tried ifelse statement for hte label but it did no recognize new.threshol as a variable for some reason
-  if(nrow(subset(pp.plot.data, RESULT_MD_X >= (Sys.time()-new.threshold) | RESULT_MD_Y >= (Sys.time()-new.threshold))) > 0)
-  {
-    p1 <- p1 + geom_text(data=subset(pp.plot.data, RESULT_MD_X >= (Sys.time()-new.threshold) | RESULT_MD_Y >= (Sys.time()-new.threshold)),
-                         aes(x=RESULT_VA_X,y=RESULT_VA_Y,color = MEDIUM_CD,label="New",hjust=1.1),show.legend=F,size=7)      
-  }
-  
-  ##highlight this water year's data
-  if(wySymbol == TRUE) 
-  {
-          p1 <- p1 + geom_point(data=subset(pp.plot.data, as.character(waterYear(SAMPLE_START_DT)) == as.character(waterYear(Sys.time()))),
-                                aes(x=RESULT_VA_X,y=RESULT_VA_Y),size=7,alpha = 0.5, color = "#F0E442",shape=19)
-  }
-  
-  if(labelDQI == TRUE)
-  {
-          p1 <- p1 + geom_text(aes(x=RESULT_VA_X,y=RESULT_VA_Y,color = MEDIUM_CD,label=paste("X-",DQI_CD_X,"_","Y-",DQI_CD_Y,sep="")),show.legend=F,size=7,vjust="bottom",hjust="right")
-  }
-  
-  p1 <- p1 + ggtitle(maintitle) + theme_bw() + theme(panel.grid.minor = element_line())
-  
-  
-  
-  if((show.lm)==TRUE){
-    p2 <- p1 + geom_smooth(data=subset(pp.plot.data, MEDIUM_CD %in%c("WS ","WG ")),aes(x=RESULT_VA_X,y=RESULT_VA_Y),method="lm",formula=y~x)
-    p2 <- p2 + xlab(paste("\n",xlabel,"\n",lm_eqn(subset(pp.plot.data, MEDIUM_CD %in%c("WS ","WG ")))))
-    
-    if(printPlot == TRUE)
-    {
-            print(p2)
-    }else{return(p2)}
-    
-    } else{if(printPlot == TRUE)
+                           site.selection,
+                           xparm,
+                           yparm,
+                           facet = "multisite",
+                           scales="fixed",
+                           new.threshold = 60*60*24*30,
+                           show.lm = FALSE,
+                           log.scaleY = FALSE,
+                           log.scaleX = FALSE,
+                           highlightrecords = " ",
+                           wySymbol = FALSE,
+                           labelDQI=FALSE,
+                           printPlot=TRUE){
+        
+        lm_eqn = function(df){
+                m = lm(RESULT_VA_Y ~ RESULT_VA_X, df);
+                eq <- paste("y =",format(coef(m)[2], digits = 2),"x +",format(coef(m)[1], digits = 2),"\nr-squared =",format(summary(m)$r.squared, digits = 3))
+                
+        }
+        
+        ## Sets color to medium code name, not factor level, so its consistant between all plots regardles of number of medium codes in data
+        medium.colors <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#D55E00")
+        names(medium.colors) <- c("WS ","WG ","WSQ","WGQ","OAQ")
+        
+        if(length(site.selection) == 1)
         {
-        print(p1)
+                #maintitle <- str_wrap(unique(qw.data$PlotTable$STATION_NM[which(qw.data$PlotTable$SITE_NO == (site.selection))]), width = 25)
+                maintitle <- unique(qw.data$PlotTable$STATION_NM[qw.data$PlotTable$SITE_NO == site.selection])
+        } else if (length(site.selection) > 1)
+        {
+                maintitle <- "Multisite parameter-parameter plot"
+        } else (maintitle <- "No site selected")
+        
+        ###Get X and Y labels from NWIS parm name
+        #xlabel <- str_wrap(unique(qw.data$PlotTable$PARM_DS[which(qw.data$PlotTable$PARM_CD==(xparm))]), width = 25)
+        xlabel <- unique(qw.data$PlotTable$PARM_DS[qw.data$PlotTable$PARM_CD == xparm])
+        #ylabel <- str_wrap(unique(qw.data$PlotTable$PARM_DS[which(qw.data$PlotTable$PARM_CD==(yparm))]), width = 25)
+        ylabel <- unique(qw.data$PlotTable$PARM_DS[qw.data$PlotTable$PARM_CD == yparm])
+        
+        ###Subset data to parms and join by record number
+        ###This is very ugly but I don't know a way to pair up the x-y data in a melted dataframe
+        ###Subsetting by parm code does not work because you need the parmcodes matched up for the same record
+        
+        xpp.plot.data <- subset(qw.data$PlotTable,SITE_NO %in% (site.selection) & PARM_CD==(xparm))
+        ypp.plot.data <- subset(qw.data$PlotTable,SITE_NO %in% (site.selection) & PARM_CD==(yparm))
+        
+        ###Assigned to global environment to make it work with ggplot2, I don't like doing this since it is not a persistant variable
+        ###but this is hte fastest fix for now
+        pp.plot.data <- dplyr::left_join(xpp.plot.data[,c("RECORD_NO","SITE_NO","STATION_NM","MEDIUM_CD","SAMPLE_START_DT","RESULT_VA","RESULT_MD","DQI_CD")], 
+                                         ypp.plot.data[,c("RECORD_NO","RESULT_VA","RESULT_MD","DQI_CD")],by="RECORD_NO")
+        names(pp.plot.data) <- c("RECORD_NO","SITE_NO","STATION_NM","MEDIUM_CD","SAMPLE_START_DT","RESULT_VA_X","RESULT_MD_X","DQI_CD_X","RESULT_VA_Y","RESULT_MD_Y","DQI_CD_Y")
+        remove(xpp.plot.data)
+        remove(ypp.plot.data)
+        
+        pp.plot.data <- na.omit(pp.plot.data)
+        ###Make the plot
+        p1 <- ggplot(data=pp.plot.data)
+        p1 <- p1 + geom_point(aes(x=RESULT_VA_X,y=RESULT_VA_Y,color = MEDIUM_CD),size=3)
+        p1 <- p1 + ylab(paste(ylabel,"\n")) + xlab(paste("\n",xlabel))
+        #Highlight records
+        if(nrow(subset(pp.plot.data, RECORD_NO %in% highlightrecords)) > 0)
+        {
+                p1 <- p1 + geom_point(data=subset(pp.plot.data,RECORD_NO %in% highlightrecords),aes(x=RESULT_VA_X,y=RESULT_VA_Y),size=7,alpha=0.5, color = "#D55E00",shape=19)
+        } else{}
+        
+        p1 <- p1 + scale_colour_manual("Medium code",values = medium.colors)
+        if ( facet == "Facet")
+        {
+                p1 <- p1 + facet_wrap(~ STATION_NM, nrow = 1, scales=scales) 
+        }else{}
+        #p1 <- p1 + stat_ellipse(aes(x=RESULT_VA_X,y=RESULT_VA_Y),level=0.999,type="t")
+        if(log.scaleY == TRUE)
+        {
+                p1 <- p1 + scale_y_log10()
+        }
+        if(log.scaleX == TRUE)
+        {
+                p1 <- p1 + scale_x_log10()
+        }
+        
+        ##Check for new samples and label them. Tried ifelse statement for hte label but it did no recognize new.threshol as a variable for some reason
+        if(nrow(subset(pp.plot.data, RESULT_MD_X >= (Sys.time()-new.threshold) | RESULT_MD_Y >= (Sys.time()-new.threshold))) > 0)
+        {
+                p1 <- p1 + geom_text(data=subset(pp.plot.data, RESULT_MD_X >= (Sys.time()-new.threshold) | RESULT_MD_Y >= (Sys.time()-new.threshold)),
+                                     aes(x=RESULT_VA_X,y=RESULT_VA_Y,color = MEDIUM_CD,label="New",hjust=1.1),show.legend=F,size=7)      
+        }
+        
+        ##highlight this water year's data
+        if(wySymbol == TRUE) 
+        {
+                p1 <- p1 + geom_point(data=subset(pp.plot.data, as.character(waterYear(SAMPLE_START_DT)) == as.character(waterYear(Sys.time()))),
+                                      aes(x=RESULT_VA_X,y=RESULT_VA_Y),size=7,alpha = 0.5, color = "#F0E442",shape=19)
+        }
+        
+        if(labelDQI == TRUE)
+        {
+                p1 <- p1 + geom_text(aes(x=RESULT_VA_X,y=RESULT_VA_Y,color = MEDIUM_CD,label=paste("X-",DQI_CD_X,"_","Y-",DQI_CD_Y,sep="")),show.legend=F,size=7,vjust="bottom",hjust="right")
+        }
+        
+        p1 <- p1 + ggtitle(maintitle) + theme_bw() + theme(panel.grid.minor = element_line())
+        
+        
+        
+        if((show.lm)==TRUE){
+                p2 <- p1 + geom_smooth(data=subset(pp.plot.data, MEDIUM_CD %in%c("WS ","WG ")),aes(x=RESULT_VA_X,y=RESULT_VA_Y),method="lm",formula=y~x)
+                p2 <- p2 + xlab(paste("\n",xlabel,"\n",lm_eqn(subset(pp.plot.data, MEDIUM_CD %in%c("WS ","WG ")))))
+                
+                if(printPlot == TRUE)
+                {
+                        print(p2)
+                }else{return(p2)}
+                
+        } else{if(printPlot == TRUE)
+        {
+                print(p1)
         }else{return(p1)}}  
 }
--- R/qwtsPlot.R
+++ R/qwtsPlot.R
@@ -43,109 +43,112 @@ qwtsPlot <- function(qw.data,
                      wySymbol = FALSE,
                      labelDQI = FALSE,
                      printPlot = TRUE){
-  ## Sets color to medium code name, not factor level, so its consistant between all plots regardles of number of medium codes in data
-  medium.colors <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#D55E00","#D55E00")
-  names(medium.colors) <- c("WS ","WG ","WSQ","WGQ","OAQ","OA ")
-  ## Sets color to medium code name, not factor level, so its consistant between all plots regardles of number of medium codes in data
-  qual.shapes <- c(19,0,2,5,4,3,6,7,8,9,11)
-  names(qual.shapes) <- c("Sample","<",">","E","A","M","N","R","S","U","V")
-
-    plotdata <- subset(qw.data$PlotTable,SITE_NO %in% (site.selection) & 
-                         PARM_CD == (plotparm))
-    Qplotdata <- subset(qw.data$PlotTable,SITE_NO %in% (site.selection) & 
-                          PARM_CD %in% c(plotparm,"00061"))
-  
-  if (length(site.selection) == 1)
-  {
-    maintitle <- stringr::str_wrap(unique(qw.data$PlotTable$STATION_NM[which(qw.data$PlotTable$SITE_NO == (site.selection))]), width = 25)
-  } else (maintitle <- "Multisite timeseries plot")
-  
-  ylabel <- stringr::str_wrap(unique(plotdata$PARM_DS[which(plotdata$PARM_CD==(plotparm))]), width = 25)
-  p1 <- ggplot(data=plotdata,aes(x=SAMPLE_START_DT,y=RESULT_VA,shape = REMARK_CD, color = MEDIUM_CD))
-  p1 <- p1 + geom_point(size=3)
-  if ( facet == "Facet")
-  {
-          p1 <- p1 + facet_wrap(~ STATION_NM, nrow = 1, scales=scales)
-  }
-  
-  p1 <- p1 + ylab(paste(ylabel,"\n")) + xlab("Date")
-  p1 <- p1 + scale_colour_manual("Medium code",values = medium.colors)
-  p1 <- p1 + scale_shape_manual("Remark code",values = qual.shapes)
-  p1 <- p1 + theme_bw()
-  p1 <- p1 + ggtitle(maintitle)
-  
-  #check for highlighted records and put different symbols
-  if(
-    nrow((data=subset(plotdata, RECORD_NO %in% highlightrecords))) > 0
-  ){
-    
-    p1 <- p1 + geom_point(data=subset(plotdata, RECORD_NO %in% highlightrecords),aes(x=SAMPLE_START_DT,y=RESULT_VA),size = 7,alpha=0.5, color ="#D55E00" ,shape=19)
-  }
-  
-  
-  ##Check for new samples and label them. 
-  if(nrow(subset(plotdata, RESULT_MD >= (Sys.time()-new.threshold))) > 0)
-  {
-    p1 <- p1 + geom_text(data=subset(plotdata, RESULT_MD >= (Sys.time()-new.threshold)),
-                         aes(x=SAMPLE_START_DT,y=RESULT_VA,color = MEDIUM_CD,label="New",hjust=1.1),show.legend=F)      
-  }else{}
-  
-  if(labelDQI == TRUE)
-  {
-          p1 <- p1 + geom_text(aes(label=DQI_CD),size=7,vjust="bottom",hjust="right")
-  }
-  
-  ##highlight this water year's data
-  if(wySymbol == TRUE)
-  {
-          p1 <- p1 + geom_point(data=subset(plotdata, as.character(waterYear(SAMPLE_START_DT)) == as.character(waterYear(Sys.time()))),
-                                aes(x=SAMPLE_START_DT,y=RESULT_VA),size=5,alpha = 0.5, color = "#F0E442",shape=19)
-  }
-  
-  p1 <- p1 + theme(axis.text.x = element_text(angle = 90)) + theme(panel.grid.minor = element_line())
-  
-  ###Add smooth if checked
-  if((show.smooth)==TRUE){
-    p1 <- p1 + geom_smooth(data = subset(plotdata, REMARK_CD=="Sample" & 
-                                           MEDIUM_CD %in%(c("WS ","WG "))))
-  } 
-  
-  
-  p3 <- ggplot(data=subset(Qplotdata, MEDIUM_CD == "WS " & 
-                                   REMARK_CD == "Sample"),aes(x=SAMPLE_START_DT,y=RESULT_VA,shape = REMARK_CD, color = MEDIUM_CD))
-  p3 <- p3 + facet_grid(PARM_NM~.,scales="free_y") + geom_point(data=subset(Qplotdata,PARM_CD == plotparm)) + geom_line(data=subset(Qplotdata,PARM_CD == "00061" & MEDIUM_CD=="WS "))
-  p3 <- p3 + theme_bw()
-  p3 <- p3 + scale_colour_manual("Medium code",values = medium.colors)
-  p3 <- p3 + scale_shape_manual("Remark code",values = qual.shapes)
-  p3 <- p3 + ylab("Value, see individual parameters for units") + xlab("Date")
-  p3 <- p3 + ggtitle(maintitle)
-  ###Add Q if checked
-  if((show.q)==TRUE){ 
-          p2 <- ggplot(data=subset(Qplotdata, MEDIUM_CD == "WS " & 
-                                           REMARK_CD == "Sample"),aes(x=SAMPLE_START_DT,y=RESULT_VA,shape = REMARK_CD, color = MEDIUM_CD))
-          p2 <- p2 + facet_grid(PARM_NM~.,scales="free_y") + geom_point(data=subset(Qplotdata,PARM_CD == plotparm)) + geom_line(data=subset(Qplotdata,PARM_CD == "00061" & MEDIUM_CD=="WS "))
-          p2 <- p2 + theme_bw()
-          p2 <- p2 + scale_colour_manual("Medium code",values = medium.colors)
-          p2 <- p2 + scale_shape_manual("Remark code",values = qual.shapes)
-          p2 <- p2 + ylab("Value, see individual parameters for units") + xlab("Date")
-          p2 <- p2 + ggtitle(maintitle)
-    if((show.smooth)==TRUE){
-      p2 <- p2 + geom_smooth(data=subset(Qplotdata,PARM_CD == plotparm & MEDIUM_CD %in%  MEDIUM_CD %in% c("WS ","WG ")))
-    }
-   
-    
-    if(printPlot == TRUE)
-    {
-      print(p2) 
-    } else(return(p2))
-    
-  }
-  if ((show.q)==FALSE)
+        ## Sets color to medium code name, not factor level, so its consistant between all plots regardles of number of medium codes in data
+        medium.colors <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#D55E00","#D55E00")
+        names(medium.colors) <- c("WS ","WG ","WSQ","WGQ","OAQ","OA ")
+        ## Sets color to medium code name, not factor level, so its consistant between all plots regardles of number of medium codes in data
+        qual.shapes <- c(19,0,2,5,4,3,6,7,8,9,11)
+        names(qual.shapes) <- c("Sample","<",">","E","A","M","N","R","S","U","V")
+        
+        plotdata <- subset(qw.data$PlotTable,SITE_NO %in% (site.selection) & 
+                                   PARM_CD == (plotparm))
+        Qplotdata <- subset(qw.data$PlotTable,SITE_NO %in% (site.selection) & 
+                                    PARM_CD %in% c(plotparm,"00061"))
+        
+        if (length(site.selection) == 1)
         {
-          if(printPlot == TRUE)
-                  {
-                  print(p1) 
-                  } else(return(p1))
-  }
-   
+                #maintitle <- stringr::str_wrap(unique(qw.data$PlotTable$STATION_NM[which(qw.data$PlotTable$SITE_NO == (site.selection))]), width = 25)
+                maintitle <- unique(qw.data$PlotTable$STATION_NM[qw.data$PlotTable$SITE_NO == site.selection])
+        } else (maintitle <- "Multisite timeseries plot")
+        
+        #ylabel <- stringr::str_wrap(unique(plotdata$PARM_DS[which(plotdata$PARM_CD==(plotparm))]), width = 25)
+        ylabel <- unique(plotdata$PARM_DS[plotdata$PARM_CD == plotparm])
+        
+        p1 <- ggplot(data=plotdata,aes(x=SAMPLE_START_DT,y=RESULT_VA,shape = REMARK_CD, color = MEDIUM_CD))
+        p1 <- p1 + geom_point(size=3)
+        if ( facet == "Facet")
+        {
+                p1 <- p1 + facet_wrap(~ STATION_NM, nrow = 1, scales=scales)
+        }
+        
+        p1 <- p1 + ylab(paste(ylabel,"\n")) + xlab("Date")
+        p1 <- p1 + scale_colour_manual("Medium code",values = medium.colors)
+        p1 <- p1 + scale_shape_manual("Remark code",values = qual.shapes)
+        p1 <- p1 + theme_bw()
+        p1 <- p1 + ggtitle(maintitle)
+        
+        #check for highlighted records and put different symbols
+        if(
+                nrow((data=subset(plotdata, RECORD_NO %in% highlightrecords))) > 0
+        ){
+                
+                p1 <- p1 + geom_point(data=subset(plotdata, RECORD_NO %in% highlightrecords),aes(x=SAMPLE_START_DT,y=RESULT_VA),size = 7,alpha=0.5, color ="#D55E00" ,shape=19)
+        }
+        
+        
+        ##Check for new samples and label them. 
+        if(nrow(subset(plotdata, RESULT_MD >= (Sys.time()-new.threshold))) > 0)
+        {
+                p1 <- p1 + geom_text(data=subset(plotdata, RESULT_MD >= (Sys.time()-new.threshold)),
+                                     aes(x=SAMPLE_START_DT,y=RESULT_VA,color = MEDIUM_CD,label="New",hjust=1.1),show.legend=F)      
+        }else{}
+        
+        if(labelDQI == TRUE)
+        {
+                p1 <- p1 + geom_text(aes(label=DQI_CD),size=7,vjust="bottom",hjust="right")
+        }
+        
+        ##highlight this water year's data
+        if(wySymbol == TRUE)
+        {
+                p1 <- p1 + geom_point(data=subset(plotdata, as.character(waterYear(SAMPLE_START_DT)) == as.character(waterYear(Sys.time()))),
+                                      aes(x=SAMPLE_START_DT,y=RESULT_VA),size=5,alpha = 0.5, color = "#F0E442",shape=19)
+        }
+        
+        p1 <- p1 + theme(axis.text.x = element_text(angle = 90)) + theme(panel.grid.minor = element_line())
+        
+        ###Add smooth if checked
+        if((show.smooth)==TRUE){
+                p1 <- p1 + geom_smooth(data = subset(plotdata, REMARK_CD=="Sample" & 
+                                                             MEDIUM_CD %in%(c("WS ","WG "))))
+        } 
+        
+        
+        p3 <- ggplot(data=subset(Qplotdata, MEDIUM_CD == "WS " & 
+                                         REMARK_CD == "Sample"),aes(x=SAMPLE_START_DT,y=RESULT_VA,shape = REMARK_CD, color = MEDIUM_CD))
+        p3 <- p3 + facet_grid(PARM_NM~.,scales="free_y") + geom_point(data=subset(Qplotdata,PARM_CD == plotparm)) + geom_line(data=subset(Qplotdata,PARM_CD == "00061" & MEDIUM_CD=="WS "))
+        p3 <- p3 + theme_bw()
+        p3 <- p3 + scale_colour_manual("Medium code",values = medium.colors)
+        p3 <- p3 + scale_shape_manual("Remark code",values = qual.shapes)
+        p3 <- p3 + ylab("Value, see individual parameters for units") + xlab("Date")
+        p3 <- p3 + ggtitle(maintitle)
+        ###Add Q if checked
+        if((show.q)==TRUE){ 
+                p2 <- ggplot(data=subset(Qplotdata, MEDIUM_CD == "WS " & 
+                                                 REMARK_CD == "Sample"),aes(x=SAMPLE_START_DT,y=RESULT_VA,shape = REMARK_CD, color = MEDIUM_CD))
+                p2 <- p2 + facet_grid(PARM_NM~.,scales="free_y") + geom_point(data=subset(Qplotdata,PARM_CD == plotparm)) + geom_line(data=subset(Qplotdata,PARM_CD == "00061" & MEDIUM_CD=="WS "))
+                p2 <- p2 + theme_bw()
+                p2 <- p2 + scale_colour_manual("Medium code",values = medium.colors)
+                p2 <- p2 + scale_shape_manual("Remark code",values = qual.shapes)
+                p2 <- p2 + ylab("Value, see individual parameters for units") + xlab("Date")
+                p2 <- p2 + ggtitle(maintitle)
+                if((show.smooth)==TRUE){
+                        p2 <- p2 + geom_smooth(data=subset(Qplotdata,PARM_CD == plotparm & MEDIUM_CD %in%  MEDIUM_CD %in% c("WS ","WG ")))
+                }
+                
+                
+                if(printPlot == TRUE)
+                {
+                        print(p2) 
+                } else(return(p2))
+                
+        }
+        if ((show.q)==FALSE)
+        {
+                if(printPlot == TRUE)
+                {
+                        print(p1) 
+                } else(return(p1))
+        }
+        
 }
